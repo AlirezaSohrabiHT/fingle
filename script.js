6238191f1e232205c4a0789a861f4f9e7223410a
Alireza <33979576+AlirezaSohrabiHT@users.noreply.github.com>
2023-07-27 13:14:35 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
-// function zoomin() {
-//     var GFG = document.getElementById("main-img");
-//     var currHeight = GFG.clientHeight;
-//         GFG.style.height = (currHeight + 40) + "px";
-//         console.log("zoomin")
-// }
-// function zoomout() {
-//     var GFG = document.getElementById("main-img");
-//     var currHeight = GFG.clientHeight;
-//         GFG.style.height = (currHeight - 40) + "px";
-//         console.log("zoomout")
 
-// }
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+if (vw<=800){
+    var element = document.getElementById("wrapper")
+    element.classList.remove("wrapper");
+}
+var myImg = document.getElementById("main-img");
+var m_width = myImg.width;
+var m_height = myImg.height;
+var body = document.getElementById("body");
+function ani(){
+    console.log(body)
+    if (body.classList.contains('angry-animate')){
+        console.log("rt")
+        body.classList.remove('angry-animate');
+    }
+    else{
+        body.classList.add('angry-animate');
+
+    }
+    }
 function zoomin(){
     var myImg = document.getElementById("main-img");
     var currWidth = myImg.clientWidth;
     if(currWidth == 2500) return false;
      else{
+        myImg.style.borderRadius = 0;
         myImg.style.width = (currWidth + 10) + "px";
         myImg.style.height = (currWidth + 10) + "px";
     } 
+    console.log(myImg.width)
 }
 function zoomout(){
     var myImg = document.getElementById("main-img");
     var currWidth = myImg.clientWidth;
-    if(currWidth == 300) return false;
-     else{
-        myImg.style.width = (currWidth - 100) + "px";
+    var currHeight = myImg.clientHeight;
+    if(currWidth <= m_width || currHeight <= m_height) {
+        myImg.style.width = m_width + "px";
+        myImg.style.height = m_height + "px";
+        myImg.style.borderRadius = 54 + "px";
+        console.log("test")
     }
+     else{
+        myImg.style.width = (currWidth - 10) + "px";
+        myImg.style.height = (currWidth - 10) + "px";
+    }
+    console.log(myImg.width)
 }
